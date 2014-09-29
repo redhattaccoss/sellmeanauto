cb83aee7d5e43400177adab2a63993c77834c732
normaneil <normaneil.macutay@gmail.com>
2014-09-29 23:39:08 +0800

    Added user_credentials_id session

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -6,9 +6,12 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
-    	//echo "<p>hello world</p>";
-		$this->view->home_page= "hello";
+    	//echo "<p>hello world</p>";exit;
 		$this->view->headScript()->appendFile("/public/js/index/index.js", "text/javascript");
+		$user_login_credentials = new Zend_Session_Namespace("user_login_credentials");
+		//echo $user_login_credentials->user_credentials_id;exit;
+		$this->view->user_credentials_id= $user_login_credentials->user_credentials_id;
+		
     }
 	
 	
