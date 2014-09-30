262d59f5c37e4f373a32ad2ac45ace198e66fd55
normaneil <normaneil.macutay@gmail.com>
2014-10-01 06:37:39 +0800

    Fixed error in index controller

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,14 +10,18 @@ class IndexController extends Zend_Controller_Action
 		$this->view->headScript()->appendFile("/public/js/index/index.js", "text/javascript");
 		$user_login_credentials = new Zend_Session_Namespace("user_login_credentials");
 		//echo $user_login_credentials->user_credentials_id;exit;
-		$db = Zend_Registry::get("main_db");
-		$sql = $db->select()
-			->from('user_profiles')
-			->where('user_credentials_id=?', $user_login_credentials->user_credentials_id );
-		$user_profiles = $db->fetchRow($sql);
+		
+		$user_profiles=array();
+		if($user_login_credentials->user_credentials_id){
+			$db = Zend_Registry::get("main_db");
+			$sql = $db->select()
+				->from('user_profiles')
+				->where('user_credentials_id=?', $user_login_credentials->user_credentials_id );
+			$user_profiles = $db->fetchRow($sql);
+			
+		}
 		
 		$this->view->user_profiles= $user_profiles;
-		
     }
 	
 	
