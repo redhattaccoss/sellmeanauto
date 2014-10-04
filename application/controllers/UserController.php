a88120a33586077a77fc32192b53a47e3354515d
normaneil <normaneil.macutay@gmail.com>
2014-10-05 00:38:39 +0800

    Updates in user profile page

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -6,22 +6,26 @@ class UserController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
-		$user_login_credentials = new Zend_Session_Namespace("user_login_credentials");
-		//echo $user_login_credentials->user_credentials_id;
-		$user_profiles=array();
-		if($user_login_credentials->user_credentials_id){
-			$db = Zend_Registry::get("main_db");
-			$sql = $db->select()
-				->from('user_profiles')
-				->where('user_credentials_id=?', $user_login_credentials->user_credentials_id );
-			$user_profiles = $db->fetchRow($sql);
-			
-		}		
-		$this->view->user_profiles= $user_profiles;
+		
     }
 
     public function indexAction()
     {
+		$user_login_credentials = new Zend_Session_Namespace("user_login_credentials");
+		if(!$user_login_credentials->user_credentials_id){
+			header("Location:/");
+			exit;
+		}
+
+
+		$db = Zend_Registry::get("main_db");
+		$sql = $db->select()
+			->from('user_profiles')
+			->where('user_credentials_id=?', $user_login_credentials->user_credentials_id );
+		$user_profiles = $db->fetchRow($sql);
+			
+		$this->view->user_profiles= $user_profiles;
+		
 		$this->view->headScript()->appendFile("/public/js/user/user.js", "text/javascript");
         $this->_helper->layout->setLayout("user");
 		
