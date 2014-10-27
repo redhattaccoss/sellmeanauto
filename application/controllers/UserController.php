d4a08705e092548f4fc6dbfa30ad6aedd12f07fb
normaneil <normaneil.macutay@gmail.com>
2014-10-27 20:04:01 +0800

    Initial commit of User portal Dashboard

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -198,6 +198,22 @@ class UserController extends Zend_Controller_Action
 		
 	}
 	
+	public function dashboardAction()
+	{
+		$user_login_credentials = new Zend_Session_Namespace("user_login_credentials");
+		$db = Zend_Registry::get("main_db");
+		
+		$sql = $db->select()
+			->from('user_profiles')
+			->where('user_credentials_id=?', $user_login_credentials->user_credentials_id );
+		$user_profiles = $db->fetchRow($sql);
+		$this->view->user_profiles= $user_profiles;		
+		$this->view->headScript()->appendFile("/public/js/user/user.js", "text/javascript");
+		$this->view->headScript()->appendFile("/public/js/user/dashboard.js", "text/javascript");
+        $this->_helper->layout->setLayout("user");
+	}
+	
+	
 	public function logoutAction()
 	{
 		Zend_Session::namespaceUnset('user_login_credentials');
---
+++ public/js/user/dashboard.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function() {
+	jQuery(window).load(function (e) {
+		console.log('Under Construction User Portal Dashboard');
+	});	
+});	
